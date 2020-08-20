16d5eb84b3677eddb3040997a33018b2c6870e2c
bundle555 <69882824+bundle555@users.noreply.github.com>
2020-08-20 20:43:32 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "login_signup.h"
+#include "menu.h"
+#include "input_util.h"
+
+#include <iostream>
+#include <string>
+
+enum startup_menu_option {
+    Login = '1',
+    Signup,
+    About,
+    Exit,
+};
+
+int main() {
+    std::cout << menu::clear_screen << menu::startup_menu;
+
+    char c{startup_menu_option::About};
+    while (c != startup_menu_option::Exit) {
+        std::cout << menu::clear_screen << menu::startup_menu;
+        c = promt_choice();
+        std::cout << menu::clear_screen;
+
+        switch (c) {
+        case startup_menu_option::Login:
+                promt_login();
+            break;
+        case startup_menu_option::Signup:
+            
+                promt_signup();
+            
+            break;
+        case startup_menu_option::About:
+            std::cout << menu::about;
+            wait_for_enter();
+            break;
+        case startup_menu_option::Exit:
+            std::cout << menu::outro;
+            return EXIT_SUCCESS;
+        default:
+            std::cout << menu::promt_invalid;
+            wait_for_enter();
+            break;
+        }
+    }
+}
