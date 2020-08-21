eb36c96924641a7f55d09c115e36f96a22b8255a
sauravj7 <sauravj271@gmail.com>
2020-08-21 11:04:48 +0530

    integrating crytofile into filecrypt

--- include/filecrypt.h
+++ include/filecrypt.h
@@ -5,26 +5,64 @@
 #include <sstream>
 #include <string>
 
+#include <cryptopp/files.h>
+#include <cryptopp/default.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/hex.h>
+
 inline void encrypt(std::string const& filename, std::string const& key) {
-    std::stringstream ss;
-    ss << std::ifstream{filename}.rdbuf();
-    std::string content = ss.str();
+    using namespace CryptoPP;
 
-    for (unsigned i = 0; i < content.size(); i++) {
-        content[i] ^= key[i % key.size()];
-    }
+    std::string encrypted;
 
-    std::ofstream{filename, std::ios::trunc} << content;
+
+    /**********************************************************
+     * As FileSource take filename in wchar_t* 
+     * The below two lines convert string to wchar_t*
+    **********************************************************/
+    auto wide_string = std::wstring(filename.begin(), filename.end());
+    const wchar_t* wfileName = wide_string.c_str();
+
+
+    FileSource ss1(wfileName, true,
+		new DefaultEncryptorWithMAC(
+			(CryptoPP::byte*)key.data(), key.size(),
+			new HexEncoder(
+				new StringSink(encrypted)
+			)
+		)
+	);
+
+
+    std::ofstream{filename, std::ios::trunc} << encrypted;
 }
 
 inline void decrypt(std::string const& filename, std::string const& key) {
-    std::stringstream ss;
-    ss << std::ifstream{filename}.rdbuf();
-    std::string content = ss.str();
+    using namespace CryptoPP;
 
-    for (unsigned i = 0; i < content.size(); i++) {
-        content[i] ^= key[i % key.size()];
-    }
+    std::string encrypted, decrypted;
 
-    std::ofstream{filename, std::ios::trunc} << content;
+    /**********************************************************
+     * Encrypted data is fetched frm Fileand kept in a string
+    **********************************************************/
+    std::ifstream{filename} >> encrypted;
+
+    /********************************************************
+     * Data being Decrypted
+    ********************************************************/
+    StringSource ss2(encrypted, true,
+		new HexDecoder(
+			new DefaultDecryptorWithMAC(
+			(CryptoPP::byte*)key.data(), key.size(),
+				new StringSink(decrypted)
+			)
+		)
+	);
+
+
+    /*******************************************************
+     * If Password Is right the Encrypted Data from the file
+     * will be deleted and the Original data will be kept.
+    *******************************************************/
+    std::ofstream{filename, std::ios::trunc} << decrypted;
 }
