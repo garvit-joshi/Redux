53979d8760e922f918214e7e86bcc568065770c9
sauravj7 <sauravj271@gmail.com>
2020-08-21 08:37:49 +0530

    adding promt num

--- include/input_util.h
+++ include/input_util.h
@@ -2,8 +2,10 @@
 
 #include "menu.h"
 
+#include <ios>
 #include <iostream>
 #include <string>
+#include <limits>
 
 inline char promt_choice() {
     std::cout << menu::promt_choice;
@@ -12,6 +14,18 @@ inline char promt_choice() {
     std::cin >> choice;
 
     return choice[0];
+}
+
+inline unsigned promt_num() {
+    std::cout << menu::promt_num;
+
+    int result;
+    while (!(std::cin >> result)) {
+        std::cout << "Wrong Input Try Again...\n" << menu::promt_num;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    return result;
 }
 
 inline void wait_for_enter() {
