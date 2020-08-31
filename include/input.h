f10db9ebb67a719e2e67619bab841d6f6cadb439
sauravj7 <sauravj271@gmail.com>
2020-08-31 22:41:41 +0530

    simplfy input

--- include/input.h
+++ include/input.h
@@ -1,16 +1,14 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+#include "str.h"
+
 #include <string>
 
 namespace input {
     auto line(char const* msg) -> std::string;
-
-    auto choice(char const* msg) -> int;
-    auto choice() -> int;
-
-    auto enter(char const* msg) -> void;
-    auto enter() -> void;
+    auto choice(char const* msg = str::choice) -> int;
+    auto enter(char const* msg = nullptr) -> void;
 } // namespace input
 
 #endif // INPUT_H
--- src/input.cpp
+++ src/input.cpp
@@ -8,7 +8,30 @@
 
 namespace input {
 
-    static int num(char const* const msg) {
+    static std::string trim(std::string const& str) {
+        std::string result;
+
+        std::string spaces{"\t\v\f "};
+        std::copy(str.begin() + str.find_first_not_of(spaces),
+                  str.begin() + str.find_last_not_of(spaces) + 1, std::back_inserter(result));
+
+        return result;
+    }
+
+    static bool valid(std::string const& str) { return !str.empty() && !trim(str).empty(); }
+
+    std::string line(char const* const msg) {
+        std::cout << msg;
+
+        std::string str;
+        while (getline(std::cin, str) && !valid(str)) {
+            std::cout << str::try_again << msg;
+        }
+
+        return trim(str);
+    }
+
+    int choice(char const* const msg) {
         std::cout << msg;
 
         int result = 0;
@@ -24,42 +47,13 @@ namespace input {
         return result;
     }
 
-    static std::string trim(std::string const& str) {
-        std::string result;
-
-        std::string spaces{"\t\v\f "};
-        std::copy(str.begin() + str.find_first_not_of(spaces),
-                  str.begin() + str.find_last_not_of(spaces) + 1,
-                  std::back_inserter(result));
-
-        return result;
-    }
-
-    static bool valid(std::string const& str) { return !str.empty() && !trim(str).empty(); }
-
-    std::string line(char const* const msg) {
-        std::string str;
-
-        std::cout << msg;
-
-        while (getline(std::cin, str) && !valid(str)) {
-            std::cout << str::try_again << msg;
+    void enter(char const* const msg) {
+        if (msg != nullptr) {
+            std::cout << msg;
         }
 
-        return trim(str);
-    }
-
-    int choice() { return num(str::choice); }
-
-    int choice(char const* const msg) { return num(msg); }
-
-    void enter(char const* const msg) {
-        std::cout << msg << str::enter;
-        std::cin.ignore();
-    }
-
-    void enter() {
         std::cout << str::enter;
         std::cin.ignore();
     }
+
 } // namespace input
