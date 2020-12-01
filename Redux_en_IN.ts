e883ec76c07891b8142edd0b3dbb123144b47807
Garvit Joshi <garvitjoshi9@gmail.com>
2020-12-02 01:09:29 +0530

    Corrected Typo

--- Redux_en_IN.ts
+++ Redux_en_IN.ts
@@ -5,37 +5,45 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
+        <location filename="build-Redux-Desktop-Debug/Redux_autogen/include/ui_mainwindow.h" line="110"/>
         <source>Redux</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="30"/>
+        <location filename="build-Redux-Desktop-Debug/Redux_autogen/include/ui_mainwindow.h" line="113"/>
         <source>Login</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="37"/>
+        <location filename="build-Redux-Desktop-Debug/Redux_autogen/include/ui_mainwindow.h" line="114"/>
         <source>Register</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="71"/>
         <location filename="mainwindow.ui" line="106"/>
+        <location filename="build-Redux-Desktop-Debug/Redux_autogen/include/ui_mainwindow.h" line="111"/>
+        <location filename="build-Redux-Desktop-Debug/Redux_autogen/include/ui_mainwindow.h" line="115"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="78"/>
+        <location filename="build-Redux-Desktop-Debug/Redux_autogen/include/ui_mainwindow.h" line="116"/>
         <source>Exit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="96"/>
+        <location filename="build-Redux-Desktop-Debug/Redux_autogen/include/ui_mainwindow.h" line="117"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="111"/>
+        <location filename="build-Redux-Desktop-Debug/Redux_autogen/include/ui_mainwindow.h" line="112"/>
         <source>License</source>
         <translation type="unfinished"></translation>
     </message>
@@ -49,8 +57,8 @@
     <message>
         <location filename="mainwindow.cpp" line="26"/>
         <location filename="mainwindow.cpp" line="38"/>
-        <source>Dear Users,&lt;br&gt;                                           &lt;b&gt;Redux&lt;/b&gt; is a cross platform applicationthat will store your password in files that will be protected with Encryption. Feel free to contact   us on any reported bug.&lt;br&gt;                        Git-Hub: &lt;a href=&apos;https://github.com/garvit-joshi/Redux&apos;&gt;Click Here&lt;/a&gt;&lt;br&gt;Developers: &lt;b&gt;Saurav Jha&lt;/b&gt;, &lt;b&gt;Garvit Joshi&lt;/b&gt;</source>
-        <oldsource>Dear Users,&lt;br&gt;                                           &lt;b&gt;Redux&lt;/b&gt; is a cross platform applicationthat will store your password in files that will be protected with Encryption. Feel free to contact   us on any reported bug.&lt;br&gt;                        Git-Hub: &lt;a href=&apos;https://github.com/garvit-joshi/Redux&apos;&gt;Click Here&lt;/a&gt;&lt;br&gt;Devlopers: &lt;b&gt;Saurav Jha&lt;/b&gt;, &lt;b&gt;Garvit Joshi&lt;/b&gt;</oldsource>
+        <source>Dear Users,&lt;br&gt;                                           &lt;b&gt;Redux&lt;/b&gt; is a cross platform open-source application that will store your password in files protected with Encryption. Feel free to contact us on any reported bug.&lt;br&gt;                          Git-Hub: &lt;a href=&apos;https://github.com/garvit-joshi/Redux&apos;&gt;Click Here&lt;/a&gt;&lt;br&gt;Developers: &lt;b&gt;Saurav Jha&lt;/b&gt;, &lt;b&gt;Garvit Joshi&lt;/b&gt;</source>
+        <oldsource>Dear Users,&lt;br&gt;                                           &lt;b&gt;Redux&lt;/b&gt; is a cross platform open-source application that will store your password in files protected with Encryption. Feel free to contact us on any reported bug.&lt;br&gt;             Git-Hub: &lt;a href=&apos;https://github.com/garvit-joshi/Redux&apos;&gt;Click Here&lt;/a&gt;&lt;br&gt;Developers: &lt;b&gt;Saurav Jha&lt;/b&gt;, &lt;b&gt;Garvit Joshi&lt;/b&gt;</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -60,8 +68,8 @@
     </message>
     <message>
         <location filename="mainwindow.cpp" line="50"/>
-        <source>MIT License&lt;br&gt;&lt;br&gt;Copyright (c) 2020 Garvit Joshi&lt;br&gt;&lt;br&gt;Permission is hereby granted, free of charge, to any person obtaining a copyof this software and associated documentation files (the &apos;Software&apos;), to dealin the Software without restriction, including without limitation the rightsto use, copy, modify, merge, publish, distribute, sublicense, and/or sellcopies of the Software, and to permit persons to whom the Software isfurnished to do so, subject to the following conditions:&lt;br&gt;&lt;br&gt;The above copyright notice and this permission notice shall be included in allcopies or substantial portions of the Software.&lt;br&gt;&lt;br&gt;THE SOFTWARE IS PROVIDED &apos;AS IS&apos;, WITHOUT WARRANTY OF ANY KIND, EXPRESS ORIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THEAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHERLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THESOFTWARE.&lt;br&gt;</source>
-        <oldsource>MIT LicenseCopyright (c) 2020 Garvit JoshiPermission is hereby granted, free of charge, to any person obtaining a copyof this software and associated documentation files (the &apos;Software&apos;), to dealin the Software without restriction, including without limitation the rightsto use, copy, modify, merge, publish, distribute, sublicense, and/or sellcopies of the Software, and to permit persons to whom the Software isfurnished to do so, subject to the following conditions:The above copyright notice and this permission notice shall be included in allcopies or substantial portions of the Software.THE SOFTWARE IS PROVIDED &apos;AS IS&apos;, WITHOUT WARRANTY OF ANY KIND, EXPRESS ORIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THEAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHERLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THESOFTWARE.</oldsource>
+        <source>MIT License&lt;br&gt;&lt;br&gt;Copyright (c) 2020 Garvit Joshi&lt;br&gt;&lt;br&gt;Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the &apos;Software&apos;), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:&lt;br&gt;&lt;br&gt;The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.&lt;br&gt;&lt;br&gt;THE SOFTWARE IS PROVIDED &apos;AS IS&apos;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.&lt;br&gt;</source>
+        <oldsource>MIT License&lt;br&gt;&lt;br&gt;Copyright (c) 2020 Garvit Joshi&lt;br&gt;&lt;br&gt;Permission is hereby granted, free of charge, to any person obtaining a copyof this software and associated documentation files (the &apos;Software&apos;), to dealin the Software without restriction, including without limitation the rightsto use, copy, modify, merge, publish, distribute, sublicense, and/or sellcopies of the Software, and to permit persons to whom the Software isfurnished to do so, subject to the following conditions:&lt;br&gt;&lt;br&gt;The above copyright notice and this permission notice shall be included in allcopies or substantial portions of the Software.&lt;br&gt;&lt;br&gt;THE SOFTWARE IS PROVIDED &apos;AS IS&apos;, WITHOUT WARRANTY OF ANY KIND, EXPRESS ORIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THEAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHERLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THESOFTWARE.&lt;br&gt;</oldsource>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,10 +24,10 @@ void MainWindow::on_pushAbout_clicked()
 {
     QMessageBox::about(this, tr("About Redux"),
              tr("Dear Users,<br>                                    "
-                "       <b>Redux</b> is a cross platform application"
-                "that will store your password in files that will be"
-                " protected with Encryption. Feel free to contact   "
-                "us on any reported bug.<br>                        "
+                "       <b>Redux</b> is a cross platform open-source"
+                " application that will store your password in files"
+                " protected with Encryption. Feel free to contact us"
+                " on any reported bug.<br>                          "
                 "Git-Hub: <a href='https://github.com/garvit-joshi/Redux\'>Click Here</a><br>"
                 "Developers: <b>Saurav Jha</b>, <b>Garvit Joshi</b>"));
 }
@@ -36,10 +36,10 @@ void MainWindow::on_actionAbout_triggered()
 {
     QMessageBox::about(this, tr("About Redux"),
              tr("Dear Users,<br>                                    "
-                "       <b>Redux</b> is a cross platform application"
-                "that will store your password in files that will be"
-                " protected with Encryption. Feel free to contact   "
-                "us on any reported bug.<br>                        "
+                "       <b>Redux</b> is a cross platform open-source"
+                " application that will store your password in files"
+                " protected with Encryption. Feel free to contact us"
+                " on any reported bug.<br>                          "
                 "Git-Hub: <a href='https://github.com/garvit-joshi/Redux\'>Click Here</a><br>"
                 "Developers: <b>Saurav Jha</b>, <b>Garvit Joshi</b>"));
 }
@@ -51,21 +51,21 @@ void MainWindow::on_actionLicense_triggered()
 
                 "Copyright (c) 2020 Garvit Joshi<br><br>"
 
-                "Permission is hereby granted, free of charge, to any person obtaining a copy"
-                "of this software and associated documentation files (the 'Software'), to deal"
-                "in the Software without restriction, including without limitation the rights"
-                "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell"
-                "copies of the Software, and to permit persons to whom the Software is"
+                "Permission is hereby granted, free of charge, to any person obtaining a copy "
+                "of this software and associated documentation files (the 'Software'), to deal "
+                "in the Software without restriction, including without limitation the rights "
+                "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell "
+                "copies of the Software, and to permit persons to whom the Software is "
                 "furnished to do so, subject to the following conditions:<br><br>"
 
-                "The above copyright notice and this permission notice shall be included in all"
+                "The above copyright notice and this permission notice shall be included in all "
                 "copies or substantial portions of the Software.<br><br>"
 
-                "THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR"
-                "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,"
-                "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE"
-                "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER"
-                "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,"
-                "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE"
+                "THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR "
+                "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, "
+                "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE "
+                "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER "
+                "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, "
+                "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE "
                 "SOFTWARE.<br>"));
 }
